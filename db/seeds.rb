230fdaa6ab293d3efb2b3e400328c70472eb7071
Alexandra James <alexandrajames@outlook.de>
2023-05-25 13:47:27 +0200

    Seed movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "json"
+require "open-uri"
+
+puts "cleaning the database"
+
+Movie.destroy_all
+
+puts "creating movies"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_result = URI.open(url).read
+movies_json = JSON.parse(movies_result)
+movies = movies_json["results"]
+
+movies.each do |movie|
+  movie = Movie.create(title: "#{movie["original_title"]}", overview: "#{movie["overview"]}", rating: "#{movie["vote_average"]}", poster_url: "https://image.tmdb.org/t/p/original/#{movie["poster_path"]}")
+  puts "Movie with #{movie.id} id was created"
+end
